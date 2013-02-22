43ba86f45aebe0dd773602110fa2fc5a61a393f2
Kalys Osmonov <kalys@osmonov.com>
2013-02-22 15:00:03 +0600

    '/etc/init.d/' instead of service bla bla

--- lib/capistrano/nginx_unicorn/tasks.rb
+++ lib/capistrano/nginx_unicorn/tasks.rb
@@ -64,7 +64,7 @@ Capistrano::Configuration.instance.load do
     %w[start stop restart].each do |command|
       desc "#{command} unicorn"
       task command, roles: :app do
-        run "service unicorn_#{application} #{command}"
+        run "/etc/init.d/unicorn_#{application} #{command}"
       end
 
       after "deploy:#{command}", "unicorn:#{command}"
