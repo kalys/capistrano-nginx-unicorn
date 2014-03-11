4ed559c7e1e1532c034e4abc431261d0e0ff69d9
Alex Agranov <alex@morphogenic.net>
2014-03-10 20:30:31 -0400

    one more via fetch()

--- lib/capistrano/nginx_unicorn/tasks.rb
+++ lib/capistrano/nginx_unicorn/tasks.rb
@@ -72,7 +72,7 @@ namespace :unicorn do
   task :setup do
     on roles(:app) do
       execute :mkdir, "-p", shared_path.join("config")
-      template "unicorn.rb.erb", unicorn_config
+      template "unicorn.rb.erb", fetch(:unicorn_config)
       template "unicorn_init.erb", "/tmp/unicorn_init"
       execute "chmod +x /tmp/unicorn_init"
       execute "#{sudo} mv /tmp/unicorn_init /etc/init.d/unicorn_#{fetch(:application)}"
