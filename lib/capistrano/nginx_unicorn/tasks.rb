10db8bb0f3e83ce0dfbd818c3e0ae5c39a46125a
Alex Agranov <alex@morphogenic.net>
2014-03-10 20:35:09 -0400

    fetch(:templates_path)

--- lib/capistrano/nginx_unicorn/tasks.rb
+++ lib/capistrano/nginx_unicorn/tasks.rb
@@ -5,7 +5,7 @@ def set_default(name, *args, &block)
 end
 
 def template(template_name, target)
-  config_file = "#{templates_path}/#{template_name}"
+  config_file = "#{fetch(:templates_path)}/#{template_name}"
   # if no customized file, proceed with default
   unless File.exists?(config_file)
     config_file = File.join(File.dirname(__FILE__), "../../generators/capistrano/nginx_unicorn/templates/#{template_name}")
